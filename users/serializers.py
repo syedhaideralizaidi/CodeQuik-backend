2f5b371d677763ced98944cf4c291bf12176593d
Syed Haider Ali Zaidi <haiderali.zaidi@conovoinc.com>
2025-03-03 11:46:31 +0500

    Intial completed

---
+++ users/serializers.py
@@ -0,0 +1,11 @@
+# from rest_framework import serializers
+#
+# class GoogleAuthResponseSerializer(serializers.Serializer):
+#     sub = serializers.CharField()
+#     email = serializers.EmailField()
+#     email_verified = serializers.BooleanField()
+#     name = serializers.CharField()
+#     picture = serializers.URLField()
+#     given_name = serializers.CharField()
+#     family_name = serializers.CharField(required=False)
+#
---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+# from .views import google_login
+
+urlpatterns = [
+    # path("auth/google-login/", google_login, name="google_login"),
+]
