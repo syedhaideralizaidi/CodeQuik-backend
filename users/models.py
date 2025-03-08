6c72dfa34d0c804bfafc523c42b942b8087ebabd
Zaida <haiderali.zaidi@conovoinc.com>
2025-03-08 10:40:24 +0500

    fix superuser

--- users/models.py
+++ users/models.py
@@ -1,41 +1,37 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractUser
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("The Email field must be set")
+
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_staff", True)
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        """
-        Create and return a superuser with email and password.
-        """
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self.create_user(email, password, **extra_fields)
-
-    def get_by_natural_key(self, email):
-        return self.get(email=email)
-
-
-class User(AbstractBaseUser):
+class User(AbstractUser):
     password = models.CharField(max_length=128, null=True, blank=True)
     first_name = models.CharField(max_length=512, blank=True, null=True)
     last_name = models.CharField(max_length=512, blank=True, null=True)
     email = models.EmailField('email address', unique=True)
+    username = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True,
+    )
     objects = UserManager()
     USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
